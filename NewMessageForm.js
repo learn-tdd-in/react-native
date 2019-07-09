9d6aa83eff481dde192f76a8130ce5268a0f31f4
Josh Justice <me@codingitwrong.com>
2019-07-09 12:45:33 -0400

    Make text input controlled

--- NewMessageForm.js
+++ NewMessageForm.js
@@ -6,11 +6,20 @@ import {
 } from 'react-native';
 
 export default class NewMessageForm extends Component {
+  state = { inputText: '' }
+
+  handleChangeText = (text) => {
+    this.setState({ inputText: text });
+  }
+
   render() {
+    const { inputText } = this.state;
     return (
       <View>
         <TextInput
+          value={inputText}
           testID="messageText"
+          onChangeText={this.handleChangeText}
         />
         <Button
           title="Send"

50542fa70d9ba0619a4b38a3003f0cb599354ca4
Josh Justice <josh.justice@accesscfa.com>
2019-11-08 13:57:28 -0500

    Fix onSend error

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import {SafeAreaView, StatusBar, View} from 'react-native';
 import NewMessageForm from './src/NewMessageForm';
 
 const App = () => {
+  const handleSend = newMessage => {};
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <View>
-          <NewMessageForm />
+          <NewMessageForm onSend={handleSend} />
         </View>
       </SafeAreaView>
     </>

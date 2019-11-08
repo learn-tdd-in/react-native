4e96c9cb9aeec8c6683aca8873b6b7896d19ab49
Josh Justice <josh.justice@accesscfa.com>
2019-11-08 13:49:49 -0500

    Add send button

--- src/NewMessageForm.js
+++ src/NewMessageForm.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {TextInput, View} from 'react-native';
+import {Button, TextInput, View} from 'react-native';
 
 const NewMessageForm = () => {
   return (
     <View>
       <TextInput testID="messageText" />
+      <Button title="Send" testID="sendButton" />
     </View>
   );
 };

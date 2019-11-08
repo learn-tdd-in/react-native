bf0129f0d0ab4f1bc4bf1915292123facaab6435
Josh Justice <josh.justice@accesscfa.com>
2019-11-08 13:46:49 -0500

    Add E2E test

---
+++ e2e/creating_a_message.spec.js
@@ -0,0 +1,14 @@
+describe('Creating a message', () => {
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should add the message to the list', async () => {
+    await element(by.id('messageText')).tap();
+    await element(by.id('messageText')).typeText('New message');
+    await element(by.id('sendButton')).tap();
+
+    await expect(element(by.id('messageText'))).toHaveText('');
+    await expect(element(by.label('New message'))).toBeVisible();
+  });
+});

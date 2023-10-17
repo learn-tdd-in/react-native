cb48a6f6a34306ff1c9199bcf6c35a713c8c5815
Josh Justice <me@codingitwrong.com>
2023-10-17 07:26:14 -0400

    Add Detox linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ['detox'],
+  overrides: [
+    {
+      files: ['e2e/*.test.js'],
+      env: {
+        'detox/detox': true,
+      },
+    },
+  ],
 };

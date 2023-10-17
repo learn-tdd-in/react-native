ff121d2deebcd1508dcff4152618137f1e41a336
Josh Justice <me@codingitwrong.com>
2023-10-17 07:40:20 -0400

    Add empty NewMessageForm

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
+import NewMessageForm from './src/NewMessageForm';
 
 export default function App() {
   return (
     <SafeAreaView>
       <StatusBar barStyle="dark-content" />
+      <NewMessageForm />
     </SafeAreaView>
   );
 }

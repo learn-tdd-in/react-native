3d47e375712eba678f9c207894219ec4b7c6178d
Josh Justice <me@codingitwrong.com>
2023-10-17 08:02:19 -0400

    Show messages in list

--- src/MessageList.tsx
+++ src/MessageList.tsx
@@ -1,7 +1,16 @@
+import React from 'react';
+import {FlatList, Text} from 'react-native';
+
 interface MessageListProps {
   messages: string[];
 }
 
 export default function MessageList({messages}: MessageListProps) {
-  return null;
+  return (
+    <FlatList
+      data={messages}
+      keyExtractor={item => item}
+      renderItem={({item}) => <Text>{item}</Text>}
+    />
+  );
 }

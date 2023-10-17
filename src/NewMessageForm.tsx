6582ee041f5caf9ff17c9eee21bbe74fe35171a3
Josh Justice <me@codingitwrong.com>
2023-10-17 07:45:00 -0400

    Clear message field

--- src/NewMessageForm.tsx
+++ src/NewMessageForm.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Pressable, Text, TextInput} from 'react-native';
 
 export default function NewMessageForm() {
+  const [inputText, setInputText] = useState('');
+
+  function handleSend() {
+    setInputText('');
+  }
+
   return (
     <>
-      <TextInput testID="messageText" />
-      <Pressable testID="sendButton">
+      <TextInput
+        testID="messageText"
+        value={inputText}
+        onChangeText={setInputText}
+      />
+      <Pressable testID="sendButton" onPress={handleSend}>
         <Text>Send</Text>
       </Pressable>
     </>
